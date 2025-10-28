1435723843f9f40e6873bdb4e7d5dbff9cdbf794
Toshkee <tosiicp@gmail.com>
2025-10-28 21:49:30 +0100

    project

--- server.js
+++ server.js
@@ -34,8 +34,7 @@ app.use(session({
 app.use('/', userController);
 app.use('/', animeController);
 
-app.listen(3000, () => {
-    console.log('Server running on port 3000');
-});
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
 
 
